64ea5bb80ec923679853bf03ee61e35bda922d79
imneerajkumar <79255742+imneerajkumar@users.noreply.github.com>
2021-03-06 20:58:01 +0530

    Add initial dicegame website files

--- dicee.js
+++ dicee.js
@@ -1,41 +1,42 @@
 //Player Name Change
 
-var player1 = "Player1";
-var player2 = "Player2";
-player1 = prompt("Add player1 name:");
-player2 = prompt("Add player2 name:");
+document.querySelector(".start").addEventListener("click", function() {
 
-document.querySelectorAll("p")[0].innerHTML = player1;
-document.querySelectorAll("p")[1].innerHTML = player2;
+  var player1 = prompt("Add player1 name:");
+  var player2 = prompt("Add player2 name:");
 
-//Add EventListener
+  document.querySelectorAll("p")[0].innerHTML = player1;
+  document.querySelectorAll("p")[1].innerHTML = player2;
 
-document.addEventListener("click", function(){
-  diceRoll();
+  diceRoll(player1, player2);
+
 });
 
-// Random image selection
+//Rolling Dicee
 
-function diceRoll(){
+function diceRoll(player1, player2) {
+  document.querySelector(".play").addEventListener("click", function() {
 
-  var randomNumber1 =  Math.floor(Math.random() * 6) + 1;
-  var imageSource1 = "images/dice" + randomNumber1 + ".png";
-  document.querySelectorAll("img")[0].setAttribute("src", imageSource1);
+    // Random image selection
 
-  var randomNumber2 =  Math.floor(Math.random() * 6) + 1;
-  var imageSource2 = "images/dice" + randomNumber2 + ".png";
-  document.querySelectorAll("img")[1].setAttribute("src", imageSource2);
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var imageSource1 = "images/dice" + randomNumber1 + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", imageSource1);
 
-  // Check for Winner
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var imageSource2 = "images/dice" + randomNumber2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", imageSource2);
 
-  if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "🚩" + player1 + " Wins!";
-  }
-  else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = player2 + " Wins!🚩";
-  }
-  else {
-    document.querySelector("h1").innerHTML = "🚩Draw!!🚩";
-  }
+    // Check for Winner
+
+    if (randomNumber1 > randomNumber2) {
+      document.querySelector("h1").innerHTML = "🚩" + player1 + " Wins!";
+    } else if (randomNumber1 < randomNumber2) {
+      document.querySelector("h1").innerHTML = player2 + " Wins!🚩";
+    } else {
+      document.querySelector("h1").innerHTML = "🚩Draw!!🚩";
+    }
+
+  });
 
 }
